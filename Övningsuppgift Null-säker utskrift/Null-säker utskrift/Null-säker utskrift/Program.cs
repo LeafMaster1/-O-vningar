7e0f4163a568954d492cd6e4c6558a0871a0d9ae
Marcus Löf <LeafMaster1@outlook.com>
2025-10-02 11:14:29 +0200

    ändering gjort null metod utan if-sats

---
+++ Övningsuppgift Null-säker utskrift/Null-säker utskrift/Null-säker utskrift/Program.cs
@@ -0,0 +1,14 @@
+﻿
+void PrintSomething(string? value)
+{
+    string input = value ?? "Det fanns inget värde att skriva ut";
+    Console.WriteLine(input);
+
+}
+
+PrintSomething(null);
+PrintSomething("");
+PrintSomething("   ");
+PrintSomething("Hello World");
+PrintSomething(null);
+PrintSomething("Test test 123");
